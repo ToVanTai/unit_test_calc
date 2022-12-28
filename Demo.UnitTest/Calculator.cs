e1ba46a3af3385081cf7190d2e4fa790b49d26aa
tovantai <tovantaidz2001@gmail.com>
2022-12-29 05:34:39 +0700

    unit test buoi 1

---
+++ Demo.UnitTest/Calculator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.App
+{
+    /// <summary>
+    /// Phép toán
+    /// </summary>
+    public class Calculator
+    {
+        /// <summary>
+        /// Tính tổng 2 số
+        /// </summary>
+        /// <param name="x">Số hạng thứ nhất</param>
+        /// <param name="y">Số hạng thứ hai</param>
+        /// <returns>Tổng 2 số</returns>
+        public long Add(long x, long y)
+        {
+            return x + y;
+        }
+
+        /// <summary>
+        /// Tính hiệu 2 số
+        /// </summary>
+        /// <param name="x">Số bị trừ</param>
+        /// <param name="y">Số trừ</param>
+        /// <returns>Hiệu 2 số</returns>
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        /// <summary>
+        /// Tính tích 2 số
+        /// </summary>
+        /// <param name="x">Thừa số thứ nhất</param>
+        /// <param name="y">Thừa số thứ hai</param>
+        /// <returns>Tích 2 số</returns>
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        /// <summary>
+        /// Tính thương 2 số
+        /// </summary>
+        /// <param name="x">Số bị chia</param>
+        /// <param name="y">Số chia</param>
+        /// <returns>Thương 2 số</returns>
+        public double Divide(double x, double y)
+        {
+            if (y.Equals(0))
+            {
+                throw new DivideByZeroException(x + " khong chia duoc cho 0");
+            }
+            else
+            {
+                return Math.Round(x / y, 3);
+            }
+
+        }
+    }
+}
---
+++ Sample.App.UnitTests/CalculatorTests.cs
@@ -0,0 +1,67 @@
+﻿namespace Sample.App.UnitTests
+{
+    internal class CalculatorTests
+    {
+        [TestCase(1, 2, 3)]
+        [TestCase(2, 3, 5)]
+        public void Add_ValidInput_ReturnsCorrectSum(long x, long y, long expectedResult)
+        {
+            //arrange - chuẩn bị đầu vào dữ liệu và kết quả mong muấn
+            var caculator = new Calculator();
+
+            //act - gọi vào hàm cần test
+            long actualResult = caculator.Add(x, y);
+
+            // assert - kiểm tra kết quả mong muấn với thực tế
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+        [Test]
+        public void Add_BillNumber_ReturnCorrectSum()
+        {
+            long x = 2000000000;
+            long y = 2000000000;
+            var expectedResult = 4000000000;
+            var caculator = new Calculator();
+            long actualResult = caculator.Add(x, y);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [TestCase(1, 2, 0.5)]
+        [TestCase(2, 1, 2)]
+        [TestCase(4, 2, 2)]
+        [TestCase(-4, 2, -2)]
+        [TestCase(-4, -2, 2)]
+        [TestCase(0, 2, 0)]
+        [TestCase(1, 3, 0.333)]
+        public void Divide_ValidInput_ReturnCorrectDevide(int x, int y, double expectedResult)
+        {
+            //arrage - chuẩn bị dữ liệu đầu vào và kết quả mong muấn
+            var caculator = new Calculator();
+
+            //act - gọi hàm cần test
+            double actualResult = caculator.Divide(x, y);
+
+            //assert - kiểm tra kết quả mong muấn với thực tế
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase(1, 0)]
+        [TestCase(-1, 0)]
+        public void Divide_DivideBy0_ThrowException(double x, double y)
+        {
+            //arrage - dữ liệu đầu vào và kết quả mong muấn
+            var calculator = new Calculator();
+
+            //act - gọi hàm cần test
+            var ex = Assert.Throws<DivideByZeroException>(() =>
+            {
+                calculator.Divide(x, y);
+            });
+            //assest - kiểm tra kết quả mong muấn với thực tế
+            Assert.AreEqual(x + " khong chia duoc cho 0", ex.Message.ToString());
+        }
+
+    }
+}
